78b3c04baf1efc197b80ea35e311746bfcbd4436
marcospsviana <marcospaulo.silvaviana@gmail.com>
2019-05-17 01:31:30 -0300

    funcao de ativamento de armario fechado

---
+++ engine/ativo.py
@@ -0,0 +1,9 @@
+import RPi.GPIO as gpio
+
+out = OUT
+in_ = IN
+board = BOARD
+
+gpio.setmode(gpio.board)
+
+gpio.setup( 
--- engine/io.py
+++ engine/io.py
@@ -1,8 +1,12 @@
 import RPi.GPIO as GPIO
 import time
+import _thread
 
 class Io(object):
     def __init__(self):
+        fechado = False
+        
+    
         def set_io(self):
             ''' definindo os pinos de entrada e saída para comandos
              e leitura de estados '''
@@ -16,30 +20,35 @@ class Io(object):
             GPIO.output(29, 0)
             GPIO.output(33, 0)
         def ativo():
-            for i in range(10):
-                GPIO.output(29, 1)
-                time.sleep(1)
-                GPIO.output(29,0)
-                time.sleep(1)
-        def ocupado():
             if GPIO.input(35) == 1:
+                while fechado == False:
+                
+                    GPIO.output(29, 1)
+                    time.sleep(0.5)
+                    GPIO.output(29,0)
+                    time.sleep(0.5)
+                    if fechado == True:
+                        break
+                    
+        def ocupado():
+            ''' leitura do pino 35 '''
+            if GPIO.input(35) == 0:
                 GPIO.output(33, 1)
+                fechado = True
             else:
                 GPIO.output(33, 0)
-        '''def desocupado():
-            for i in range(5):
-                GPIO.output(31,1)
-                time.sleep(1)
-                GPIO.output(31,0)'''
+                fechado = False
+                
+        
                 
             
         set_io(self)
         ativo()
-        time.sleep(1)
+        #time.sleep(1)
         ocupado()
         #time.sleep(1)
         #desocupado()
-        GPIO.cleanup()
+        #GPIO.cleanup()
             
 if __name__ == '__main__':
     while True:
