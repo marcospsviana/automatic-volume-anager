94e0317a9eafbf24d04525c93a0671726f987a55
marcospsviana <marcospaulo.silvaviana@gmail.com>
2019-06-12 22:16:03 -0300

    view webview

--- view.py
+++ view.py
@@ -1,13 +1,14 @@
 import webview
-from flask import Flask
+from flask import Flask, url_for
 from rasp import app
 
 class View(object):
     def __init__(self):
         
-       
+        app.run()
         webview.create_window('','http://127.0.0.1:5000', frameless=False)
-        webview.load_html('', "app.run(host='localhost', port=5000)")
+        webview.load_url('http://127.0.0.1:5000/index')
+        
         
 
 if __name__ == "__main__":
