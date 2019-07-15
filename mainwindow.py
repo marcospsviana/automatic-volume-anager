77b9ead395288efd95a074fdbb24fcf3a73f812a
marcospsviana <marcospaulo.silvaviana@gmail.com>
2019-07-15 16:18:47 -0300

    interfaces gtk

--- mainwindow.py
+++ mainwindow.py
@@ -3,13 +3,25 @@ gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 
 class Handler:
+    
     def onDestroy(self, *args):
         Gtk.main_quit()
+    
+    def on_btn_locar_clicked(self, *args):
+        armarios.run()
+           
+
+
 
 builder = Gtk.Builder()
 builder.add_from_file("main_raspcontrol.glade")
 
+builder.connect_signals(Handler())
+
+
 window = builder.get_object("main_window")
+
+
 window.show_all()
 
 Gtk.main()
