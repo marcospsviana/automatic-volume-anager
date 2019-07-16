f9cb4229c3725094ee31fe2921df4f61120f0639
marcospsviana <marcospaulo.silvaviana@gmail.com>
2019-07-15 23:14:38 -0300

    add janela armarios

--- mainwindow.py
+++ mainwindow.py
@@ -1,27 +1,42 @@
 import gi
 gi.require_version('Gtk', '3.0')
-from gi.repository import Gtk
+from gi.repository import Gtk, Gdk
 
-class Handler:
+class RaspControl(object):
+    def __init__(self):
+        builder = Gtk.Builder()
+        builder.add_from_file("index.glade")
+        builder.connect_signals({
+        "btn_locar_clicked_cb": self.btn_locar_clicked_cb,
+        "gtk_widget_destroy": self.gtk_widget_destroy,
+        "on_onpen": self.abrir,
+        "gtk_main_quit": Gtk.main_quit
+        })
+        self.window = builder.get_object("main_window")
+        self.locar = builder.get_object("locar_window")
+        self.window.show()
+        
+
+       
     
-    def onDestroy(self, *args):
-        Gtk.main_quit()
     
-    def on_btn_locar_clicked(self, *args):
-        armarios.run()
+    def gtk_widget_destroy(self, widget):
+        self.locar.hide()
+    
+    def btn_locar_clicked_cb(self, widget):
+        self.locar.show()
+
+        
+        
+    def abrir(self, widget):
+        pass
+    
            
 
 
 
-builder = Gtk.Builder()
-builder.add_from_file("main_raspcontrol.glade")
-
-builder.connect_signals(Handler())
 
 
-window = builder.get_object("main_window")
-
-
-window.show_all()
-
-Gtk.main()
+if __name__ == "__main__":
+    app = RaspControl()
+    Gtk.main()
