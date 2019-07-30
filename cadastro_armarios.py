9e6dabe93a1619980caa19b3b4f4ad259b4379ad
marcospsviana <marcospaulo.silvaviana@gmail.com>
2019-07-30 12:24:02 -0300

    interface encerrar, layout teclado modificado

--- cadastro_armarios.py
+++ cadastro_armarios.py
@@ -1,14 +1,45 @@
 import gi 
 gi.require_version('Gtk', '3.0')
-from gi.repository import Gtk
+from gi.repository import Gtk, Gdk
+from controllers import Management
 
 class MainWindowCad():
     def __init__(self, *args, **kwargs):
+        builder = Gtk.Builder()
+        builder.add_from_file("cadastros_armarios.glade")
+        builder.connect_signals(
+            {
+                "gtk_main_quit": Gtk.main_quit,
+                "on_btn_cadastrar_armario_button_press_event": self.on_btn_cadastrar_armario_button_press_event,
+            }
+        )
         
-        window = Gtk.Window(title="Cadastro de Armários")
-        window.connect('destroy', Gtk.main_quit)
-        window.show()
-        Gtk.main()
+        self.window_cad = builder.get_object("cad_armarios_window")
+        self.combo_classe = builder.get_object("combobox_classe")
+        self.combo_coluna = builder.get_object("combobox_coluna")
+        self.combo_nivel = builder.get_object("combobox_nivel")
+        self.combo_terminal = builder.get_object("combobox_terminal")
+        self.btn_cadastrar = builder.get_object("btn_cadastrar_armario")
+        self.btn_cadastrar.connect("clicked", self.on_btn_cadastrar_armario_button_press_event)
+        self.window_cad.show()
+
+    def on_btn_cadastrar_armario_button_press_event(self, event):
+        CLASSES = ["A", "B", "C", "D"]
+        NIVEIS = ["SUPERIOR", "INFERIOR", "SUPERIOR DIREITA", "SUPERIOR ESQUERDA", "INFERIOR DIREITA", "INFERIOR ESQUERDA"]
+        COLUNAS = list(map(lambda x: x, range(1,17)))
+        TERMINAIS = ["OTHON PALACE HOTEL", "PORTO FUTURO HOTEL"]
+        self.classe = CLASSES[self.combo_classe.get_active()]
+        self.nivel = NIVEIS[self.combo_nivel.get_active()]
+        self.coluna = COLUNAS[self.combo_coluna.get_active()]
+        self.terminal = TERMINAIS[self.combo_terminal.get_active()]
+        print(self.classe, self.nivel, self.coluna, self.terminal)
+        manager = Management()
+        result = manager.cad_armarios( self.classe, self.terminal, self.coluna, self.nivel)
+        self.lbl_resultado.set_text(result)
+
+
+        
 
 if __name__ == "__main__":
-    MainWindowCad()
+    app = MainWindowCad()
+    Gtk.main()
--- encerrar.py
+++ encerrar.py
@@ -10,6 +10,7 @@ class Encerrar(object):
         self.manager = Management()
         self.value = ''
         self.entrada = ''
+        self.num = False
         builder = Gtk.Builder()
         builder.add_from_file("encerrar.glade")
         self.alfa = list(string.ascii_lowercase) # alfabeto para gerar o teclado
@@ -21,6 +22,7 @@ class Encerrar(object):
                 "on_entry_nome": self.on_entry_nome,
                 "on_entry_senha": self.on_entry_senha,
                 "on_btn_login_clicked": self.on_btn_login_clicked,
+                "on_num_button_press_event": self.on_num_button_press_event,
                 "gtk_widget_destroy" : self.gtk_widget_destroy
             }
         )
@@ -31,12 +33,62 @@ class Encerrar(object):
         self.entry_nome.connect("button-press-event", self.on_entry_nome)
         self.entry_senha.connect("button-press-event", self.on_entry_senha)
         #adicionando os elementos do teclado =======================
-        for a in self.alfa:
-            self.a = builder.get_object("%s"%a)
-            self.a.connect("clicked", self.on_entry_button_press_event)
-        for n in self.num:
+        #for a in self.alfa:
+        self.a = builder.get_object("a")
+        self.a.connect("clicked", self.on_entry_button_press_event)
+        self.b = builder.get_object("b")
+        self.b.connect("clicked", self.on_entry_button_press_event)
+        self.c = builder.get_object("c")
+        self.c.connect("clicked", self.on_entry_button_press_event)
+        self.d = builder.get_object("d")
+        self.d.connect("clicked", self.on_entry_button_press_event)
+        self.e = builder.get_object("e")
+        self.e.connect("clicked", self.on_entry_button_press_event)
+        self.f = builder.get_object("f")
+        self.f.connect("clicked", self.on_entry_button_press_event)
+        self.g = builder.get_object("g")
+        self.g.connect("clicked", self.on_entry_button_press_event)
+        self.h = builder.get_object("h")
+        self.h.connect("clicked", self.on_entry_button_press_event)
+        self.i = builder.get_object("i")
+        self.i.connect("clicked", self.on_entry_button_press_event)
+        self.j = builder.get_object("j")
+        self.j.connect("clicked", self.on_entry_button_press_event)
+        self.k = builder.get_object("k")
+        self.k.connect("clicked", self.on_entry_button_press_event)
+        self.l = builder.get_object("l")
+        self.l.connect("clicked", self.on_entry_button_press_event)
+        self.m = builder.get_object("m")
+        self.m.connect("clicked", self.on_entry_button_press_event)
+        self.n = builder.get_object("n")
+        self.n.connect("clicked", self.on_entry_button_press_event)
+        self.o = builder.get_object("o")
+        self.o.connect("clicked", self.on_entry_button_press_event)
+        self.p = builder.get_object("p")
+        self.p.connect("clicked", self.on_entry_button_press_event)
+        self.q = builder.get_object("q")
+        self.q.connect("clicked", self.on_entry_button_press_event)
+        self.r = builder.get_object("r")
+        self.r.connect("clicked", self.on_entry_button_press_event)
+        self.s = builder.get_object("s")
+        self.s.connect("clicked", self.on_entry_button_press_event)
+        self.t = builder.get_object("t")
+        self.t.connect("clicked", self.on_entry_button_press_event)
+        self.u = builder.get_object("u")
+        self.u.connect("clicked", self.on_entry_button_press_event)
+        self.v = builder.get_object("v")
+        self.v.connect("clicked", self.on_entry_button_press_event)
+        self.w = builder.get_object("w")
+        self.w.connect("clicked", self.on_entry_button_press_event)
+        self.x = builder.get_object("x")
+        self.x.connect("clicked", self.on_entry_button_press_event)
+        self.y = builder.get_object("y")
+        self.y.connect("clicked", self.on_entry_button_press_event)
+        self.z = builder.get_object("z")
+        self.z.connect("clicked", self.on_entry_button_press_event)
+        '''for n in self.num:
             self.n = builder.get_object("%s"%n)
-            self.n.connect("clicked", self.on_entry_button_press_event)
+            self.n.connect("clicked", self.on_entry_button_press_event)'''
 
         self.arroba = builder.get_object("arroba")
         self.dot = builder.get_object("dot")
@@ -49,12 +101,14 @@ class Encerrar(object):
         self.space = builder.get_object("space")
         self.btn_delete = builder.get_object("DELETE")
         self.btn_login = builder.get_object("btn_login")
-        self.enter = builder.get_object("ENTER")
-        self.enter.connect("clicked", self.on_ENTER_clicked)
+        #self.enter = builder.get_object("ENTER")
+        #self.enter.connect("clicked", self.on_ENTER_clicked)
         self.lbl_time = builder.get_object("time")
         self.window_time = builder.get_object("window_time")
         self.dialog_cobranca = builder.get_object("dialog_cobranca")
         self.lbl_message = builder.get_object("lbl_message")
+        self.btn_num = builder.get_object("num")
+
         #========== fim elementos do teclado =====================
         #conectando os botões aos eventos ========================
         self.btn_delete.connect("clicked", self.on_entry_backspace)
@@ -71,6 +125,35 @@ class Encerrar(object):
         
 
         self.window_login_encerrar.show()
+    def on_num_button_press_event(self, widget, event):
+        if self.num == False:
+            self.num = True
+            self.a.set_label('a')
+            self.b.set_label('b')
+            self.c.set_label('c')
+            self.d.set_label('d')
+            self.e.set_label('e')
+            self.f.set_label('f')
+            self.g.set_label('g')
+            self.h.set_label('h')
+            self.i.set_label('i')
+            self.j.set_label('j')
+            self.btn_num.set_label('123..')
+        else:
+            self.num = False
+            self.a.set_label('0')
+            self.b.set_label('1')
+            self.c.set_label('2')
+            self.d.set_label('3')
+            self.e.set_label('4')
+            self.f.set_label('5')
+            self.g.set_label('6')
+            self.h.set_label('7')
+            self.i.set_label('8')
+            self.j.set_label('9')
+            self.btn_num.set_label('abc..')
+            
+        
         
     def on_ENTER_clicked(self, widget):
         self.window_time.show()
@@ -154,6 +237,29 @@ class Encerrar(object):
         self.dialog_cobranca.destroy()
     def on_window_login_encerrar_destroy(self, widget):
         self.window_login_encerrar.close()
+    
+    def gtk_style(self):
+        css = b"""
+        #btn_locar { color: #000000;  font-size: 32px;}
+        #btn_abrir { color: #000000;  font-size: 32px;}
+        #btn_encerrar { color: #000000;  font-size: 32px;}
+        
+        #btn_num{ background-color: red; font-size: 22px}
+        #grid_teclado { font-size: 15px}
+        #btn_proximo { font-size: 20px; background-color: #008cc3; color: #fff }
+        #btn_cancelar { font-size: 20px; background-color: red; color: #fff }
+        #label { font-size: 22px; }
+        #entry { font-size: 22px; color: #000000}
+        #lbl_time { font-size: 52px }       
+        #tecla { font-size: 27px;}
+        """
+        style_provider = Gtk.CssProvider()
+        style_provider.load_from_data(css)
+        Gtk.StyleContext.add_provider_for_screen(
+            Gdk.Screen.get_default(),
+            style_provider,
+            Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION
+        )
 
 if __name__ == "__main__":
     Encerrar()
