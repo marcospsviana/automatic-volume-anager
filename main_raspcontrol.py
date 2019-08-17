24e40421590c8b5cbfedffaea1ec35f6614c3c78
marcospsviana <marcospaulo.silvaviana@gmail.com>
2019-08-17 20:29:18 -0300

    alteracoes layouts

--- main_raspcontrol.py
+++ main_raspcontrol.py
@@ -8,8 +8,9 @@ import gi
 import numpy as np
 import string
 import encodings.unicode_escape
-gi.require_version('Gtk', '3.0')
-from gi.repository import Gtk, Gdk
+gi.require_versions({'Gtk': '3.0', 'GLib': '2.0'})
+from gi.repository import Gtk, Gdk, GLib
+from datetime import datetime, date
 from controllers import Management
 from login import Login
 from encerrar import Encerrar
@@ -26,8 +27,18 @@ class RaspControl(object):
             "on_btn_principal_clicked": self.on_btn_principal_clicked,
         })
         self.main_window = self.builder.get_object("mainwindow")
+        self.label_horario = self.builder.get_object("label_horario")
+        self.label_data = self.builder.get_object("label_data")
+        GLib.timeout_add(1000, self.hora_certa)
         self.main_window.fullscreen()
         self.main_window.show()
+    
+    def hora_certa(self):
+        dia = datetime.now()
+        self.label_horario.set_text(str(dia.hour)+ ":" + str(dia.minute) + ":" + str(dia.second))
+        self.label_data.set_text(str(dia.day)+"/"+str(dia.month))
+        return (self.label_data,self.label_horario)
+
 
 
     def on_btn_principal_clicked(self, event):
--- select_cabinet.py
+++ select_cabinet.py
@@ -3,13 +3,12 @@ gi.require_versions({'Gtk': '3.0', 'GLib': '2.0', 'Gio': '2.0'})
 from gi.repository import Gtk, Gdk
 from datetime import datetime, timedelta
 import time
-from gi.repository import GObject 
+from gi.repository import GLib 
 
 from window_op_hora_diaria import OpcaoHoraDiaria
 
 class SelectCabinet(object):
     def __init__(self):
-        self.condicao = True
         self.build = Gtk.Builder()
         self.build.add_from_file("ui/escolha.glade")
         self.build.connect_signals(
@@ -22,7 +21,7 @@ class SelectCabinet(object):
         self.btn_reservar = self.build.get_object("btn_reservar")
         self.label_horario = self.build.get_object("label_horario")
         self.label_data = self.build.get_object("label_data")
-        GObject.timeout_add(1000, self.hora_certa )
+        GLib.timeout_add(1000, self.hora_certa )
             
        
         self.select_cabinet.fullscreen()
@@ -43,9 +42,6 @@ class SelectCabinet(object):
 
 
 if __name__ == "__main__":
-    
     app = SelectCabinet()
-    
-    self.condicao = True
     Gtk.main(hora_certa())
     
