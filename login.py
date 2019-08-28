c1937a991282049feea105b6bc3433225162f197
marcospsviana <marcospaulo.silvaviana@gmail.com>
2019-08-27 23:37:35 -0300

    entrada dados login

--- login.py
+++ login.py
@@ -9,110 +9,104 @@ class Login(Gtk.Window):
     def __init__(self):
         #self.gtk_style()    
         self.screen = Gdk.Screen.get_default()
-        builder = Gtk.Builder()
-        builder.add_from_file("ui/login.glade")
+        self.builder = Gtk.Builder()
+        self.builder.add_from_file("ui/login.glade")
         self.manager = Management()
         self.value = ''
         self.entrada = ''
         self.alfa = list(string.ascii_lowercase) # alfabeto para gerar o teclado
         self.num = list(map(lambda x: x, range(10))) # números para o teclado numérico
-        builder.connect_signals(
+        self.builder.connect_signals(
             {
                 "on_window_login_destroy": self.on_window_login_destroy,
                 "on_ENTER_clicked": self.on_ENTER_clicked,
                 "on_entry_email_telefone_button_press_event": self.on_entry_email_telefone_button_press_event,
                 "on_entry_senha_button_press_event": self.on_entry_senha_button_press_event,
                 "on_btn_login_clicked": self.on_btn_login_clicked,
-                "on_num_button_press_event": self.on_num_button_press_event,
                 "on_cancela_clicked": self.on_cancela_clicked,
                 "gtk_widget_destroy" : self.gtk_widget_destroy
             }
         )
-        self.window_login = builder.get_object("window_login")
+        self.window_login = self.builder.get_object("window_login")
+        self.window_entrada_dados = self.builder.get_object("window_entrada_dados")
         # adicionando elementos da janela
-        self.entry_nome = builder.get_object("entry_email_telefone")
+        self.entry_nome = self.builder.get_object("entry_email_telefone")
         
-        self.entry_senha = builder.get_object('entry_senha')
+        self.entry_senha = self.builder.get_object('entry_senha')
         self.entry_nome.connect("button-press-event", self.on_entry_email_telefone_button_press_event)
         self.entry_senha.connect("button-press-event", self.on_entry_senha_button_press_event)
-        '''#adicionando os elementos do teclado =======================
-        self.a = builder.get_object("a")
+        #adicionando os elementos do teclado =======================
+        self.a = self.builder.get_object("a")
         self.a.connect("clicked", self.on_entry_button_press_event)
-        self.b = builder.get_object("b")
+        self.b = self.builder.get_object("b")
         self.b.connect("clicked", self.on_entry_button_press_event)
-        self.c = builder.get_object("c")
+        self.c = self.builder.get_object("c")
         self.c.connect("clicked", self.on_entry_button_press_event)
-        self.d = builder.get_object("d")
+        self.d = self.builder.get_object("d")
         self.d.connect("clicked", self.on_entry_button_press_event)
-        self.e = builder.get_object("e")
+        self.e = self.builder.get_object("e")
         self.e.connect("clicked", self.on_entry_button_press_event)
-        self.f = builder.get_object("f")
+        self.f = self.builder.get_object("f")
         self.f.connect("clicked", self.on_entry_button_press_event)
-        self.g = builder.get_object("g")
+        self.g = self.builder.get_object("g")
         self.g.connect("clicked", self.on_entry_button_press_event)
-        self.h = builder.get_object("h")
+        self.h = self.builder.get_object("h")
         self.h.connect("clicked", self.on_entry_button_press_event)
-        self.i = builder.get_object("i")
+        self.i = self.builder.get_object("i")
         self.i.connect("clicked", self.on_entry_button_press_event)
-        self.j = builder.get_object("j")
+        self.j = self.builder.get_object("j")
         self.j.connect("clicked", self.on_entry_button_press_event)
-        self.k = builder.get_object("k")
+        self.k = self.builder.get_object("k")
         self.k.connect("clicked", self.on_entry_button_press_event)
-        self.l = builder.get_object("l")
+        self.l = self.builder.get_object("l")
         self.l.connect("clicked", self.on_entry_button_press_event)
-        self.m = builder.get_object("m")
+        self.m = self.builder.get_object("m")
         self.m.connect("clicked", self.on_entry_button_press_event)
-        self.n = builder.get_object("n")
+        self.n = self.builder.get_object("n")
         self.n.connect("clicked", self.on_entry_button_press_event)
-        self.o = builder.get_object("o")
+        self.o = self.builder.get_object("o")
         self.o.connect("clicked", self.on_entry_button_press_event)
-        self.p = builder.get_object("p")
+        self.p = self.builder.get_object("p")
         self.p.connect("clicked", self.on_entry_button_press_event)
-        self.q = builder.get_object("q")
+        self.q = self.builder.get_object("q")
         self.q.connect("clicked", self.on_entry_button_press_event)
-        self.r = builder.get_object("r")
+        self.r = self.builder.get_object("r")
         self.r.connect("clicked", self.on_entry_button_press_event)
-        self.s = builder.get_object("s")
+        self.s = self.builder.get_object("s")
         self.s.connect("clicked", self.on_entry_button_press_event)
-        self.t = builder.get_object("t")
+        self.t = self.builder.get_object("t")
         self.t.connect("clicked", self.on_entry_button_press_event)
-        self.u = builder.get_object("u")
+        self.u = self.builder.get_object("u")
         self.u.connect("clicked", self.on_entry_button_press_event)
-        self.v = builder.get_object("v")
+        self.v = self.builder.get_object("v")
         self.v.connect("clicked", self.on_entry_button_press_event)
-        self.w = builder.get_object("w")
+        self.w = self.builder.get_object("w")
         self.w.connect("clicked", self.on_entry_button_press_event)
-        self.x = builder.get_object("x")
+        self.x = self.builder.get_object("x")
         self.x.connect("clicked", self.on_entry_button_press_event)
-        self.y = builder.get_object("y")
+        self.y = self.builder.get_object("y")
         self.y.connect("clicked", self.on_entry_button_press_event)
-        self.z = builder.get_object("z")
+        self.z = self.builder.get_object("z")
         self.z.connect("clicked", self.on_entry_button_press_event)
         
-        self.space = builder.get_object("space")
-        self.btn_delete = builder.get_object("DELETE")
-        self.btn_login = builder.get_object("btn_login")
-        #self.enter = builder.get_object("ENTER")
+        self.space = self.builder.get_object("space")
+        #self.btn_delete = self.builder.get_object("DELETE")
+        self.btn_login = self.builder.get_object("btn_login")
+        #self.enter = self.builder.get_object("ENTER")
         #self.enter.connect("clicked", self.on_ENTER_clicked)
-        self.lbl_time = builder.get_object("time")
-        self.window_time = builder.get_object("window_time")
-        self.dialog_cobranca = builder.get_object("dialog_cobranca")
-        self.lbl_message = builder.get_object("lbl_message")
-        self.btn_num = builder.get_object("num")
-        self.space = builder.get_object("space")
-        self.btn_delete = builder.get_object("DELETE")
-        self.btn_login = builder.get_object("btn_login")
-        self.lbl_time = builder.get_object("time")
-        self.window_time = builder.get_object("window_time")
-        self.dialog_cobranca = builder.get_object("dialog_cobranca")
-        self.lbl_message = builder.get_object("lbl_message")
+        self.lbl_time = self.builder.get_object("time")
+        self.window_time = self.builder.get_object("window_time")
+        self.dialog_cobranca = self.builder.get_object("dialog_cobranca")
+        self.lbl_message = self.builder.get_object("lbl_message")
+        self.btn_num = self.builder.get_object("num")
+        
         
 
         #========== fim elementos do teclado =====================
         #conectando os botões aos eventos ========================
-        self.btn_delete.connect("clicked", self.on_entry_backspace)
+        #self.btn_delete.connect("clicked", self.on_entry_backspace)
         #conectando os botões aos eventos ========================
-        self.btn_delete.connect("clicked", self.on_entry_backspace)
+        #self.btn_delete.connect("clicked", self.on_entry_backspace)
         #s = Gdk.Screen.get_default()'''
         #self.window_login.fullscreen()
         self.window_login.show()
@@ -121,66 +115,7 @@ class Login(Gtk.Window):
         self.window_login.hide()
     
         
-    def on_num_button_press_event(self, widget, event):
-        if self.num == False:
-            self.num = True
-            self.a.set_label('a')
-            self.b.set_label('b')
-            self.c.set_label('c')
-            self.d.set_label('d')
-            self.e.set_label('e')
-            self.f.set_label('f')
-            self.g.set_label('g')
-            self.h.set_label('h')
-            self.i.set_label('i')
-            self.j.set_label('j')
-            self.k.set_label('k')
-            self.l.set_label('l')
-            self.m.set_label('m')
-            self.n.set_label('n')
-            self.o.set_label('o')
-            self.p.set_label('p')
-            self.q.set_label('q')
-            self.r.set_label('r')
-            self.s.set_visible(True)
-            self.t.set_visible(True)
-            self.u.set_visible(True)
-            self.v.set_visible(True)
-            self.w.set_visible(True)
-            self.x.set_visible(True)
-            self.y.set_visible(True)
-            self.z.set_visible(True)
-            
-            self.btn_num.set_label('123..')
-        else:
-            self.num = False
-            self.a.set_label('0')
-            self.b.set_label('1')
-            self.c.set_label('2')
-            self.d.set_label('3')
-            self.e.set_label('4')
-            self.f.set_label('5')
-            self.g.set_label('6')
-            self.h.set_label('7')
-            self.i.set_label('8')
-            self.j.set_label('9')
-            self.k.set_label('@')
-            self.l.set_label('_')
-            self.m.set_label('-')
-            self.n.set_label('.')
-            self.o.set_label('.com')
-            self.p.set_label('@gmail')
-            self.q.set_label('@outlook')
-            self.r.set_label('@yahoo')
-            self.s.set_visible(False)
-            self.t.set_visible(False)
-            self.u.set_visible(False)
-            self.v.set_visible(False)
-            self.w.set_visible(False)
-            self.x.set_visible(False)
-            self.y.set_visible(False)
-            self.z.set_visible(False)
-            self.btn_num.set_label('abc..')
+   
             
         
     def on_ENTER_clicked(self, widget):
@@ -217,8 +152,9 @@ class Login(Gtk.Window):
         #self.dialog_cobranca.show()
         print('cobranca em dialogo')   
     def on_entry_email_telefone_button_press_event(self, widget, event):
-        self.entrada = '7'
-        return self.entrada
+        #self.entrada = '7'
+        #return self.entrada
+        self.window_entrada_dados.show()
     
     def on_entry_senha_button_press_event(self, widget, event):
         self.entrada = '8'
@@ -264,25 +200,7 @@ class Login(Gtk.Window):
     def on_window_login_destroy(self, widget):
         self.window_login.close()
 
-    '''def gtk_style(self):
-        css = b"""
-        *{ font-size: 27px;}        
-        #grid_teclado { font-size: 15px}
-        #btn_cancelar { font-size: 20px; background-color: red; color: #fff }
-        #label { font-size: 25px; }
-        #lbl_time { font-size: 52px }       
-        #label_telefone { color: #fff }
-        #label_senha { color: #fff }
-        #window { background-color: #000; color: #fff;}
-        #entry { font-size: 32px}
-        """
-        style_provider = Gtk.CssProvider()
-        style_provider.load_from_data(css)
-        Gtk.StyleContext.add_provider_for_screen(
-            Gdk.Screen.get_default(),
-            style_provider,
-            Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION
-        )'''
+    
 
 if __name__ == "__main__":
     Login()
