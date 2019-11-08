3d8495d1b21429769ba34c83d115b0b51635bb26
marcospsviana <marcospaulo.silvaviana@gmail.com>
2019-11-08 11:19:25 -0300

    alter select port data.py

--- engine/data.py
+++ engine/data.py
@@ -785,10 +785,13 @@ ENGINE=InnoDB;''')
     
     @classmethod
     def select_port(self, armario):
+         __conn = mdb.connect(
+            user='coolbaguser', password='m1cr0@t805i', database='coolbag')
+        __c = __conn.cursor(buffered=True)
         __armario = armario
-        self.__c.execute("SELECT porta FROM coolbag.tb_armario where id_armario = %s"%(__armario))
-        self.retorno_porta = self.__c.fetchall()
-        return self.retorno_porta
+        __c.execute("SELECT porta FROM coolbag.tb_armario where id_armario = %s"%(__armario))
+        retorno_porta = __c.fetchall()
+        return retorno_porta
 
 
 
