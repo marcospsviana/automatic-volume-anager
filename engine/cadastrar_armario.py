3839ca41009b6c5b6187baff4ce823bd2d7e34ed
marcospsviana <marcospaulo.silvaviana@gmail.com>
2019-11-10 19:16:23 -0300

    remove antigo cadastrar_armario

--- engine/cadastrar_armario.py
+++
@@ -1,8 +0,0 @@
-# -*- encoding: utf-8 -*-
-import os
-import sys
-sys.path.insert(0,"/home/marcos/projeto_coolbag/raspcontrol/")
-import webview
-
-webview.create_window('', 'http://localhost:5000/cad_armario')
-
