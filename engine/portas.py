6518061a2ccd5ab5b0fac502341e12d5ac3acb7d
marcospsviana <marcospaulo.silvaviana@gmail.com>
2019-11-11 12:07:10 -0300

    alterado return antes de arbrir em self.abrir armario

--- engine/portas.py
+++ engine/portas.py
@@ -78,7 +78,7 @@ class Portas(object):
         print(__exec)
         __exec = b'%b'%(__exec.encode('utf-8'))
         self.serial.write(__exec)
-        sleep(15)
+        time.sleep(15)
         self.port = b'%b'%(self.port.encode('utf-8'))
         comando = self.port + b":fecha\n" 
         self.serial.write(comando)
