60d29b752355fb69e8ea209b3f971761c742de0b
marcospsviana <marcospaulo.silvaviana@gmail.com>
2019-11-11 16:01:57 -0300

    alteracao thread data py

--- engine/data.py
+++ engine/data.py
@@ -708,7 +708,8 @@ ENGINE=InnoDB;''')
                 porta = self.select_port(self.__locacao['id_armario'][0])
                 print("abrir armario data.py porta", str(porta[0][0]))
                 #self.port.exec_port(porta[0][0], "abre")
-                PortasThreading(porta[0][0])
+                t = threading.Thread(target=self.port.exec_port(porta[0][0]]))
+                t.start()
                 return "armario liberado"
             else:
                 query_data_locacao = "select data_locacao from tb_locacao where senha = '%s'"%__senha
@@ -832,4 +833,4 @@ class PortasThreading(threading.Thread):
 
 if __name__ == "__main__":
     Banco()
-    PortasThreading.start()
+    
