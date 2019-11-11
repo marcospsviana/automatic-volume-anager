895a7c72761769032f27cd03f60636e70f0fd30f
marcospsviana <marcospaulo.silvaviana@gmail.com>
2019-11-11 07:31:13 -0300

    alterado exec ports para async teste

--- engine/data.py
+++ engine/data.py
@@ -707,7 +707,7 @@ ENGINE=InnoDB;''')
                 
                 porta = self.select_port(self.__locacao['id_armario'][0])
                 print("abrir armario data.py porta", str(porta[0][0]))
-                self.port.exec_port(porta[0][0], "abre")
+                await self.port.exec_port(porta[0][0], "abre")
                 return "armario liberado"
             else:
                 query_data_locacao = "select data_locacao from tb_locacao where senha = '%s'"%__senha
