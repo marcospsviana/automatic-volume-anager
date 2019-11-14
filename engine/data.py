89223de14bfd85f03a89082fdc5d31c6197fba1e
marcospsviana <marcospaulo.silvaviana@gmail.com>
2019-11-14 10:44:03 -0300

    alterar __porta para str em fechar armario data.py

--- engine/data.py
+++ engine/data.py
@@ -827,7 +827,7 @@ ENGINE=InnoDB;''')
         print("id armario em fechar armario data.py", __id_armario)
         __porta = self.select_port(__id_armario[0][0])
         print("porta select porta id_armario", __porta)
-        porta.exec_port(__porta[0][0], "fecha")
+        porta.exec_port(str(__porta[0][0]), "fecha")
         return "fechado"
         
 
