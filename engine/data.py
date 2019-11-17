c6ccb63040a70ef33a5777f30db59fe2d662017d
marcospsviana <marcospaulo.silvaviana@gmail.com>
2019-11-17 13:43:12 -0300

    __armario em portas alterado

--- engine/data.py
+++ engine/data.py
@@ -169,8 +169,8 @@ class Banco(object):
         # seleciona um armario com a classe indicada e recebe seu id
         loca_armario = self.localisa_armario(self.__armario)
 
-        # print('======= loca ramario =====')
-        # print(loca_armario)
+        print('======= loca ramario =====')
+        print(loca_armario)
         # self.__c.execute("SET FOREIGN_KEY_CHECKS = 0;")
         # se houver armário livre segue com cadastro de locação
         retorno = self.pagamento_locacao(self.__total)
@@ -823,10 +823,10 @@ class Banco(object):
         __conn = mdb.connect(
             user='coolbaguser', password='m1cr0@t805i', database='coolbag')
         __c = __conn.cursor(buffered=True)
-        __armario = armario
-        print("__ARMARIO EM SELECT_port ", __armario[0][0])
+        __armario = armario[0][0]
+        print("__ARMARIO EM SELECT_port ", __armario)
         __c.execute(
-            "select porta from tb_armario where id_armario='%s'" % (__armario[0][0]))
+            "select porta from tb_armario where id_armario='%s'" % (__armario))
         retorno_porta = __c.fetchall()
         __conn.close()
         return retorno_porta
