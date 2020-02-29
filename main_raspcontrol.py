9771bcf2d0cd6a455587877926d5e5629d2283c3
marcospsviana <marcospaulo.silvaviana@gmail.com>
2020-02-29 01:58:19 -0300

    nova tela select_hora

--- main_raspcontrol.py
+++ main_raspcontrol.py
@@ -494,132 +494,6 @@ class OpcaoHoraDiaria(object):
         self.label_por_diaria.set_text("DAILY")
         self.btn_tela_hora_diaria.set_label("RETURN TO THE PREVIOUS SCREEN")
 
-class WindowSelectHora:
-    def __init__(self, *args):
-        #teste = args
-        #print("op diaria", teste)
-        #self.classe = args[0]
-        #self.language = args[1]
-        self.tempo_locacao = args[0]
-        self.classe = args[1][0]
-        self.language = args[2]
-        if self.classe == "A" and self.tempo_locacao == "horas":
-            self.taxa = TaxAndRates.TAXA_HORA_A.value
-        elif self.classe == "B" and self.tempo_locacao == "horas":
-            self.taxa = TaxAndRates.TAXA_HORA_B.value
-        elif self.classe == "C" and self.tempo_locacao == "horas":
-            self.taxa = TaxAndRates.TAXA_HORA_C.value
-        elif self.classe == "D" and self.tempo_locacao == "horas":
-            self.taxa = TaxAndRates.TAXA_HORA_D.value
-        locale.setlocale(locale.LC_ALL, "en_US.UTF-8")
-        self.builder = Gtk.Builder()
-        self.gtk_style_calendario()
-        self.builder.add_from_file("ui/window_select_horas.glade")
-        self.builder.connect_signals(
-            {
-                "gtk_main_quit"             : self.on_window_select_horas_destroy,
-                "on_btn_button_press_event" : self.on_btn_button_press_event,
-                "on_bnt_confirmar_select_horas_button_press_event": self.on_bnt_confirmar_select_horas_button_press_event,
-                "on_btn_cancelar_select_hora_button_press_event": self.on_btn_cancelar_select_hora_button_press_event,
-            }
-        )
-
-
-        self.window_select_horas = self.builder.get_object("window_select_horas")
-
-        self.bnt_confirmar_select_horas = self.builder.get_object("bnt_confirmar_select_horas")
-        #self.bnt_confirmar_select_horas.connect("button_press_event", self.on_bnt_confirmar_select_horas_button_press_event)
-        self.btn_cancelar_select_hora = self.builder.get_object("btn_cancelar_select_hora")
-        #self.btn_cancelar_select_hora.connect("button_press_event", self.on_btn_cancelar_select_hora_button_press_event)
-
-
-        self.btn1 = self.builder.get_object('btn1')
-        self.btn2 = self.builder.get_object('btn2')
-        self.btn3 = self.builder.get_object('btn3')
-        self.btn4 = self.builder.get_object('btn4')
-        self.btn5 = self.builder.get_object('btn5')
-        self.btn6 = self.builder.get_object('btn6')
-        self.btn7 = self.builder.get_object('btn7')
-        self.btn8 = self.builder.get_object('btn8')
-        self.btn9 = self.builder.get_object('btn9')
-        self.btn10 = self.builder.get_object('btn10')
-        self.btn11 = self.builder.get_object('btn11')
-        self.btn12 = self.builder.get_object('btn12')
-        self.btn13 = self.builder.get_object('btn13')
-        self.btn14 = self.builder.get_object('btn14')
-        self.btn15 = self.builder.get_object('btn15')
-        self.btn16 = self.builder.get_object('btn16')
-        self.btn17 = self.builder.get_object('btn17')
-        self.btn18 = self.builder.get_object('btn18')
-        self.btn19 = self.builder.get_object('btn19')
-        self.btn20 = self.builder.get_object('btn20')
-        self.btn21 = self.builder.get_object('btn21')
-        self.btn22 = self.builder.get_object('btn22')
-        self.btn23 = self.builder.get_object('btn23')
-        self.btn24 = self.builder.get_object('btn24')
-        self.btn1.connect("button_press_event", self.on_btn_button_press_event)
-        self.btn2.connect("button_press_event", self.on_btn_button_press_event)
-        self.btn3.connect("button_press_event", self.on_btn_button_press_event)
-        self.btn4.connect("button_press_event", self.on_btn_button_press_event)
-        self.btn5.connect("button_press_event", self.on_btn_button_press_event)
-        self.btn6.connect("button_press_event", self.on_btn_button_press_event)
-        self.btn7.connect("button_press_event", self.on_btn_button_press_event)
-        self.btn8.connect("button_press_event", self.on_btn_button_press_event)
-        self.btn9.connect("button_press_event", self.on_btn_button_press_event)
-        self.btn10.connect("button_press_event", self.on_btn_button_press_event)
-        self.btn11.connect("button_press_event", self.on_btn_button_press_event)
-        self.btn12.connect("button_press_event", self.on_btn_button_press_event)
-        self.btn13.connect("button_press_event", self.on_btn_button_press_event)
-        self.btn14.connect("button_press_event", self.on_btn_button_press_event)
-        self.btn15.connect("button_press_event", self.on_btn_button_press_event)
-        self.btn16.connect("button_press_event", self.on_btn_button_press_event)
-        self.btn17.connect("button_press_event", self.on_btn_button_press_event)
-        self.btn18.connect("button_press_event", self.on_btn_button_press_event)
-        self.btn19.connect("button_press_event", self.on_btn_button_press_event)
-        self.btn20.connect("button_press_event", self.on_btn_button_press_event)
-        self.btn21.connect("button_press_event", self.on_btn_button_press_event)
-        self.btn22.connect("button_press_event", self.on_btn_button_press_event)
-        self.btn23.connect("button_press_event", self.on_btn_button_press_event)
-        self.btn24.connect("button_press_event", self.on_btn_button_press_event)
-
-        self.btn24.set_name("focado")
-
-        self.label_valor_total_value = self.builder.get_object("label_valor_total_value")
-
-
-        self.window_select_horas.show()
-    def on_btn_button_press_event(self, widget,  event):
-        self.hora = int(widget.get_label())
-        valor_total = self.hora * self.taxa
-        self.label_valor_total_value.set_text("%.2f"%valor_total)
-        return self.hora
-
-    def on_bnt_confirmar_select_horas_button_press_event(self, widget, event):
-        self.total = self.label_valor_total_value.get_text()
-        dia = 0
-        self.window_select_horas.hide()
-        CadastroUsuarios(self.total , self.language, self.classe, dia, self.hora)
-    
-    def on_btn_cancelar_select_hora_button_press_event(self, widget, event):
-        self.label_valor_total_value.set_text("")
-        self.window_select_horas.hide()
-        OpcaoHoraDiaria(self.classe, self.language)
-        
-    def on_window_select_horas_destroy(self):
-        self.window_select_horas.hide()
-
-    def gtk_style_calendario(self):
-        css = b"""
-        
-        @import url("static/css/calendario.css");
-        """
-        style_provider = Gtk.CssProvider()
-        style_provider.load_from_data(css)
-        Gtk.StyleContext.add_provider_for_screen(
-            Gdk.Screen.get_default(),
-            style_provider,
-            Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION
-        )
 
 
 class SelectHora(object):
@@ -641,7 +515,7 @@ class SelectHora(object):
         self.gtk_style_calendario()
         self.diretorio = os.getcwd()
         self.builder = Gtk.Builder()
-        self.builder.add_from_file("ui/selec_hora_window.glade")
+        self.builder.add_from_file("ui/w_select_hora.glade")
         self.builder.connect_signals({
             "gtk_main_quit": Gtk.main_quit,
             "on_btn_diminui_hora_button_press_event": self.on_btn_diminui_hora_button_press_event,
@@ -652,7 +526,7 @@ class SelectHora(object):
             "on_btn_aumenta_minuto_button_press_event": self.on_btn_aumenta_minuto_button_press_event,
 
             })
-        self.select_hora_window = self.builder.get_object("select_hora_window")
+        self.select_hora_window = self.builder.get_object("w_select_hora")
         self.btn_diminui_hora = self.builder.get_object("btn_diminui_hora")
         self.btn_aumenta_hora = self.builder.get_object("btn_aumenta_hora")
         self.btn_diminui_minuto = self.builder.get_object("btn_diminui_minuto")
@@ -661,8 +535,8 @@ class SelectHora(object):
         self.btn_confirmar_select_hora = self.builder.get_object("btn_confirmar_select_hora")
 
         self.label_hora_ecolhida = self.builder.get_object("label_hora_ecolhida")
-        self.label_hora_ecolhida_up = self.builder.get_object("label_hora_ecolhida_up")
-        self.label_hora_ecolhida_down = self.builder.get_object("label_hora_ecolhida_down")
+        #self.label_hora_ecolhida_up = self.builder.get_object("label_hora_ecolhida_up")
+        #s#elf.label_hora_ecolhida_down = self.builder.get_object("label_hora_ecolhida_down")
         #self.label_minuto_ecolhido = self.builder.get_object("label_minuto_ecolhido")
         #self.gtk_treeview_lista_horas = self.builder.get_object("gtk_treeview_lista_horas")
         self.label_data_hora_inicial = self.builder.get_object("label_data_hora_inicial")
@@ -671,9 +545,11 @@ class SelectHora(object):
         self.data_atual = datetime.now()
         hora = self.data_atual + timedelta(hours=1)
         self.hora_acrescida = [hora.hour , self.data_atual.minute]
-        self.label_hora_ecolhida_up.set_text("2 horas")
+        #self.label_hora_ecolhida_up.set_text("2 horas")
+        self.btn_aumenta_hora.set_label("23 horas")
         self.label_hora_ecolhida.set_text("1 hora")
-        self.label_hora_ecolhida_down.set_text("23 horas")
+        #self.label_hora_ecolhida_down.set_text("23 horas")
+        self.btn_diminui_hora.set_label("2 horas")
         #self.label_minuto_ecolhido.set_text(self.data_atual.strftime("%M"))
         self.label_data_hora_inicial.set_text(self.data_atual.strftime("%d/%m/%Y - %H:%M"))
         self.label_data_hora_final.set_text(hora.strftime("%d/%m/%Y - %H:%M"))
@@ -772,12 +648,25 @@ class SelectHora(object):
 
         hora = self.horas[hora_total]
         print("hora", hora)
-        hora -= 2 
+        #hora -= 2 
         print("hora -1", hora)
+        """if hora == 22:
+                                    hora = 0
+                                else:"""
+        hora -=  2
 
         self.label_hora_ecolhida.set_text(self.hora_labels[hora])
-        self.label_hora_ecolhida_up.set_text(self.hora_labels[hora + 1])
-        self.label_hora_ecolhida_down.set_text(self.hora_labels[hora - 1])
+        """if self.label_hora_ecolhida_up.get_label() == "23 horas":
+                                    self.label_hora_ecolhida_up.set_text(self.hora_labels[0])
+                                else:
+                                    self.label_hora_ecolhida_up.set_text(self.hora_labels[hora + 1])"""
+        #if self.btn_aumenta_hora.get_label() == "23 horas":
+        #self.btn_aumenta_hora.set_label(self.hora_labels[0])
+        #else:
+        self.btn_aumenta_hora.set_label(self.hora_labels[hora -1])
+
+        #self.label_hora_ecolhida_down.set_text(self.hora_labels[hora - 1])
+        self.btn_diminui_hora.set_label(self.hora_labels[hora + 1])
 
         total_atual = self.label_valor_total_horas.get_label()
         total_atual = total_atual.replace(",",".")
@@ -802,8 +691,14 @@ class SelectHora(object):
         else:
             hora +=   1
         self.label_hora_ecolhida.set_text(self.horas_ad[hora])
-        self.label_hora_ecolhida_up.set_text(self.hora_labels[hora + 1])
-        self.label_hora_ecolhida_down.set_text(self.hora_labels[hora - 1])
+        #self.label_hora_ecolhida_up.set_text(self.hora_labels[hora + 1])
+        #self.label_hora_ecolhida_down.set_text(self.hora_labels[hora - 1])
+        if self.label_hora_ecolhida.get_label() == "23 horas":
+            self.btn_diminui_hora.set_label(self.hora_labels[0])
+            self.btn_aumenta_hora.set_label(self.hora_labels[hora - 1])
+        else:
+            self.btn_diminui_hora.set_label(self.hora_labels[hora + 1])
+            self.btn_aumenta_hora.set_label(self.hora_labels[hora - 1])
 
         total = float(self.horas[self.label_hora_ecolhida.get_label()]) * self.taxa #+ float(total_atual)
         total = str("%.2f"%total)
