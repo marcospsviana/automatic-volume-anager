77b532f4ba41cb1f76a85858a3634204e0167a1e
marcospsviana <marcospaulo.silvaviana@gmail.com>
2020-03-01 16:50:01 -0300

    design reformulado e imports taxa em dataaccess

--- engine/DataAccessObjects.py
+++ engine/DataAccessObjects.py
@@ -1,6 +1,6 @@
 from coolbagsafe_system.data_access_objets_db_cbs import DataAccessObjectsBase as DAO
 from coolbagsafe_system.portas import Portas
-from taxas import *
+from coolbagsafe.taxas import *
 from .TransacsAndOps import TransacsOps
 import sys, os
 import asyncio
